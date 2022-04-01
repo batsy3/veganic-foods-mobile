ceec94e6b07b70608e1ec8453ac803339caa9453
David utibe-abasi okuku <dvdokuku@gmail.com>
2022-04-01 14:57:54 +0200

    bqckground and backbutton

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const bGcolor = Color(0xff98C43D);
---
+++ lib/paymentPage/paymentPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:veganic_foods_app/constants.dart';
+import 'package:veganic_foods_app/paymentPage/components/background.dart';
+class PaymentPage extends StatelessWidget {
+  const PaymentPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: bGcolor,
+      body: BackGround(
+        
+      ),
+      
+      
+    );
+  }
+}
